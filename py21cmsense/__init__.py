a1d232c2df0324a55924b8f9d170b04e51968fc9
Steven Murray <steven.g.murray@asu.edu>
2023-07-27 13:56:01 -0700

    test: add tests of approximate conversions

--- py21cmsense/__init__.py
+++ py21cmsense/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("21cmSense")
-except PackageNotFoundError:
+except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
