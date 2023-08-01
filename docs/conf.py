05d24a11aa6629716b13af5ff88aee423dda15f6
Steven Murray <steven.g.murray@asu.edu>
2023-08-01 15:19:49 -0700

    docs: fix furo sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ html_theme = "furo"
 html_use_smartypants = True
 html_last_updated_fmt = "%b %d, %Y"
 html_split_index = False
-html_sidebars = {"**": ["searchbox.html", "globaltoc.html", "sourcelink.html"]}
+html_sidebars = {"**": ["searchbox.html", "sourcelink.html"]}
 html_short_title = "%s-%s" % (project, version)
 
 napoleon_use_ivar = True
